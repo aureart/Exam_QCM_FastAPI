1f1bd477094cd55a2c02409d7e643d58b6fe07a8
Ubuntu <ubuntu@ip-172-31-23-45.eu-west-1.compute.internal>
2024-01-25 16:45:12 +0000

    ajout questions

--- api.py
+++ api.py
@@ -47,3 +47,30 @@ async def authenticate_user(username: str = Depends(auth.get_current_username)):
     return {
                 'results': username
     }
+
+# -----------------------------------------------
+@app.post("/add_question")
+async def post_question(
+         question: str, subject: str, correct: str, use: str,\
+         responseA: str, responseB: str, responseC: str, responseD: Optional[str] = None, \
+         username: str = Depends(auth.is_admin)) :
+    #valid_subjects = db.get_subjects()
+    #valid_uses = db.get_uses()
+    #if subject not in valid_subjects:
+    #    raise ValueError("Le sujet n'est pas valide.")
+    #if use is not None and use not in valid_uses:
+    #    raise ValueError("L'utilisation spécifiée n'est pas valide.")
+    request = qcm.add_question(question, subject, correct, use, \
+                              responseA, responseB, responseC, responseD)
+    if request :        
+        return {
+
+                   'results' : {
+                               'username': username,
+                               'question':question, 'subject':subject, 'correct':correct,
+                               'use':use, 'responseA': responseA, 'responseB': responseB,
+                               'responseC': responseC, 'responseD': responseD
+                   }
+         }
+    else : 
+        return { 'message d\'erreur un peu con'}
