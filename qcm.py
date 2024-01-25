e6093642774a43bf214e3fd479880e3d651f2089
Ubuntu <ubuntu@ip-172-31-23-45.eu-west-1.compute.internal>
2024-01-25 16:45:22 +0000

    ajout questions

--- qcm.py
+++ qcm.py
@@ -5,10 +5,12 @@
 #
 
 import data as db
+from typing import Optional
 import random
 
+##retourne un booleen ok =1
 def add_question(question: str, subject: str, use: str, correct: str, \
-                 responseA: str, responseB: str, responseC: str, responseD: str) -> bool:
+                 responseA: str, responseB: str, responseC: str, responseD: Optional[str] = None) -> bool:
     request = db.add_question(question, subject, use, correct, \
                               responseA, responseB, responseC, responseD)
     return request 
