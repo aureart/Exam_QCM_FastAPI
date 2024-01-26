b777ab8beb48f49a30f85c7e11f2275ad283beb9
Ubuntu <ubuntu@ip-172-31-23-45.eu-west-1.compute.internal>
2024-01-26 11:55:21 +0000

    ajout exception

--- api.py
+++ api.py
@@ -51,26 +51,34 @@ async def authenticate_user(username: str = Depends(auth.get_current_username)):
 # -----------------------------------------------
 @app.post("/add_question")
 async def post_question(
-         question: str, subject: str, correct: str, use: str,\
-         responseA: str, responseB: str, responseC: str, responseD: Optional[str] = None, \
-         username: str = Depends(auth.is_admin)) :
-    #valid_subjects = db.get_subjects()
-    #valid_uses = db.get_uses()
-    #if subject not in valid_subjects:
-    #    raise ValueError("Le sujet n'est pas valide.")
-    #if use is not None and use not in valid_uses:
-    #    raise ValueError("L'utilisation spécifiée n'est pas valide.")
-    request = qcm.add_question(question, subject, correct, use, \
-                              responseA, responseB, responseC, responseD)
-    if request :        
-        return {
+    question: str, subject: str, correct: str, use: str,
+    responseA: str, responseB: str, responseC: str, responseD: Optional[str] = None,
+    username: str = Depends(auth.is_admin)
+):
 
-                   'results' : {
-                               'username': username,
-                               'question':question, 'subject':subject, 'correct':correct,
-                               'use':use, 'responseA': responseA, 'responseB': responseB,
-                               'responseC': responseC, 'responseD': responseD
-                   }
-         }
-    else : 
-        return { 'message d\'erreur un peu con'}
+    # Validation des sujets et utilisations
+    valid_subjects = db.get_subjects()
+    valid_uses = db.get_uses()
+    if subject not in valid_subjects:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Le sujet n'est pas valide.")
+    if use not in valid_uses:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="L'utilisation spécifiée n'est pas valide.")
+
+    # Tentative d'ajout de la question
+    try:
+        request = qcm.add_question(question, subject, correct, use, responseA, responseB, responseC, responseD)
+        if not request:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Erreur lors de l'ajout de la question.")
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+    # Réponse en cas de succès
+    return {
+        'results': {
+            'username': username,
+            'question': question, 'subject': subject, 'correct': correct,
+            'use': use, 'responseA': responseA, 'responseB': responseB,
+            'responseC': responseC, 'responseD': responseD
+        }
+    }
+
